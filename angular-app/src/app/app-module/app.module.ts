6ada2d5a8330bed8dee1a46fa87df6fe46076f0f
Nikishkin Sergey <nikishkinsy@gmail.com>
2020-04-03 14:59:58 +0300

    add module to app-module

--- angular-app/src/app/app-module/app.module.ts
+++ angular-app/src/app/app-module/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
 
-import { AppRoutingModule } from '../app-routing.module';
-import { SignupModule } from '../feature-modules/signup/signup.module';
-import { SigninModule } from '../feature-modules/signin/signin.module';
-import { PublicModule } from '../feature-modules/public/public.module';
-import { PrivateModule } from '../feature-modules/private/private.module';
+import { AppRoutingModule } from '../app-routing.module'
+import { SignupModule } from '../feature-modules/signup/signup.module'
+import { SigninModule } from '../feature-modules/signin/signin.module'
+import { PublicModule } from '../feature-modules/public/public.module'
+import { PrivateModule } from '../feature-modules/private/private.module'
+import { ConfirmationModule } from '../feature-modules/confirmation/confirmation.module'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
@@ -15,11 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     SignupModule,
     SigninModule,
     PublicModule,
-    PrivateModule
+    PrivateModule,
+    ConfirmationModule
   ],
   providers: [],
   bootstrap: [AppComponent]

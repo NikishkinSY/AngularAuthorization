e2053bd15e2942c9990455e82e73d2617c683834
Nikishkin Sergey <nikishkinsy@gmail.com>
2020-04-05 11:39:14 +0300

    edit confirmation service

--- angular-app/src/app/feature-modules/confirmation/confirmation.component.ts
+++ angular-app/src/app/feature-modules/confirmation/confirmation.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core'
 import { ApiService } from 'src/app/core-module/api.service'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-public',
   templateUrl: './confirmation.component.html'
 })
 export class ConfirmationComponent implements OnInit {
+  email: string
+  guid: string
 
-  constructor(private apiService: ApiService) {}
+  public error: string
+  public info: string
+
+  constructor(private apiService: ApiService, private actRoute: ActivatedRoute) {
+    this.email = this.actRoute.snapshot.params.email;
+    this.guid = this.actRoute.snapshot.params.guid;
+  }
 
   ngOnInit() {
+    this.apiService.confirmation(this.email, this.guid).subscribe({
+      next: (response: string) => { 
+        console.log(response) 
+        this.info = response
+      },
+      error: response => { 
+        console.log(response) 
+        this.error = response.error
+      }
+    });
   }
 }
